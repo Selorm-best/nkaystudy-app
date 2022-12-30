5b7615699d48fff729a5204ad293c36baa10eb4a
Selorm-best <83176869+Selorm-best@users.noreply.github.com>
2022-12-30 08:26:54 +0000

    deleted courses1

--- courses1/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-#from courses.models import Courses,Lesson,Program
-# Register your models here.
-from courses.models import Lendet,Lesson,klasa
-admin.site.register(Lendet)
-admin.site.register(Lesson)
-admin.site.register(klasa)
