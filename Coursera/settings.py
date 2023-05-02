dbd762d1f3b160e3acc90436a86c5aeb7629fb21
Selorm-best <83176869+Selorm-best@users.noreply.github.com>
2023-05-02 22:17:06 +0000

    A few change in settings

--- Coursera/settings.py
+++ Coursera/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     'blog',
     'crispy_forms',
     'allauth',
+    'Coursera'
 ]
 
 MIDDLEWARE = [
@@ -101,21 +102,40 @@ WSGI_APPLICATION = 'Coursera.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-if DEVELOPMENT_MODE is True:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+# if DEVELOPMENT_MODE is True:
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.sqlite3",
+#             "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+#         }
+#     }
+# elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+#     if os.getenv("DATABASE_URL", None) is None:
+#         raise Exception("DATABASE_URL environment variable not defined")
+#     DATABASES = {
+#         "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+#     }
+
+if os.getenv("DATABASE_URL","") != "":
+    r = urlparse(os.environ.get("DATABASE_URL"))
+    DATABASES ={
+        "default":{
+            "ENGINE" : "django.db.backends.postgresql_psycopg2",
+            "NAME" : os.path.relpath(r.path,"/"),
+            "USER" : r.username,
+            "PASSWORD" : r.password,
+            "HOST" : r.hostname,
+            "PORT": r.port,
+            "OPTIONS": {"sslmode":"require"},
         }
     }
-elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
-    if os.getenv("DATABASE_URL", None) is None:
-        raise Exception("DATABASE_URL environment variable not defined")
+else:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+        'default':{
+            'ENGINE':'django.db.backends.sqlite3',
+            'NAME':BASE_DIR+'/db.sqlite3',
+        }
     }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
